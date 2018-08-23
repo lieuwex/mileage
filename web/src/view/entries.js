7d3bb07c6d17e9a7fd5569fe7829be487156f2e6
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2018-08-23 17:01:08 +0200

    some more code cleanups

--- web/src/view/entries.js
+++ web/src/view/entries.js
@@ -9,37 +9,36 @@ import {
 } from './utils.js';
 
 const entryElem = entry => {
-	const makeRow = (key, value) => {
-		return html`
-			<div class="row">
-				<b>${key}</b>: ${value}
-			</div>
-		`;
-	};
+	const [ friendlyType, rows ] = (function () {
+		const makeRow = (key, value) =>
+			html`
+				<div class="row">
+					<b>${key}</b>: ${value}
+				</div>
+			`;
 
-	let friendlyType;
-	let rows;
-	if (entry.type === 'mileage') {
-		const duration = entry.endDate - entry.beginDate;
-		const dateRow = sameDate(entry.beginDate, entry.endDate) ?
-			formatDate(entry.beginDate) :
-			`${formatDate(entry.beginDate)} - ${formatDate(entry.endDate)}`;
+		switch (entry.type) {
+		case 'mileage': {
+			const duration = entry.endDate - entry.beginDate;
+			const dateRow = sameDate(entry.beginDate, entry.endDate) ?
+				formatDate(entry.beginDate) :
+				`${formatDate(entry.beginDate)} - ${formatDate(entry.endDate)}`;
 
-		friendlyType = 'Trip';
-		rows = [
-			makeRow('Date', dateRow),
-			makeRow('Duration', formatDuration(duration / 1000)),
-			makeRow('Distance Driven', `${entry.distance}km`),
-		];
-	} else if (entry.type === 'payment') {
-		friendlyType = 'Payment';
-		rows = [
-			makeRow('Date', formatDateTime(entry.endDate)),
-			makeRow('Price', '€' + entry.price.toLocaleString(locale)),
-		]
-	} else {
+			return [ 'Trip', [
+				makeRow('Date', dateRow),
+				makeRow('Duration', formatDuration(duration / 1000)),
+				makeRow('Distance Driven', `${entry.distance}km`),
+			]];
+		}
+		case 'payment':
+			return [ 'Payment', [
+				makeRow('Date', formatDateTime(entry.endDate)),
+				makeRow('Price', '€' + entry.price.toLocaleString(locale)),
+			]];
+		}
+
 		throw new Error(`unkown type '${entry.type}'`);
-	}
+	})();
 
 	return html`
 		<div class$="entry ${entry.type}">
@@ -54,11 +53,12 @@ const entryElem = entry => {
 	`;
 };
 
-export const entriesList = state => html`
-	<div id="entries">
-		${state.get('loadingEntries') ?
-			html`<span class="loading">Loading entries...</span>` :
-			entries(state).map(entryElem).reverse()
-		}
-	</div>
-`;
+export const entriesList = state =>
+	html`
+		<div id="entries">
+			${state.get('loadingEntries') ?
+				html`<span class="loading">Loading entries...</span>` :
+				entries(state).map(entryElem).reverse()
+			}
+		</div>
+	`;
